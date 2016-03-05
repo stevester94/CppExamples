92f051632b04d57c5c845a4af8819f7c579ba9d9
Steven Mackey <stevester94@gmail.com>
2016-03-04 18:08:29 -0800

    add static

---
+++ static.cpp
@@ -0,0 +1,57 @@
+/* Static
+Static variables within a function are kept the same across all function calls
+Static variables within a class are shared across all instantiations of it.
+	Have a very weird initialization process
+Static functions are called from the class not an object
+*/
+
+#include <iostream>
+using namespace std;
+
+void staticCallCounter() {
+	static int numCalls = 0; //Preserved across calls
+
+	cout << numCalls << endl;
+
+	numCalls++;
+}
+
+class StaticClass {
+public:
+	StaticClass() {
+		staticVar++;
+
+	}
+
+	void report() {
+		cout << staticVar << endl;
+	}
+
+	static void resetStaticVar() {
+		staticVar = 0;
+	}
+
+	static int staticVar; //This must be initialized outside of the class!
+};
+
+int StaticClass::staticVar = 10; //This is the proper way to initialize
+
+int main() {
+
+	cout << "staticCallCounter" << endl;
+	for(int i = 0; i < 10; i++) {
+		staticCallCounter();
+	}
+
+	StaticClass::resetStaticVar(); // How you access static functions
+	
+	//This becomes valid once we initialize outside of class. 
+	StaticClass::staticVar = 1337; 
+	cout << endl << "staticClass stuff" << endl;
+	for(int i = 0; i < 10; i++) {
+		StaticClass sc;
+		sc.report();
+	}
+
+
+}
